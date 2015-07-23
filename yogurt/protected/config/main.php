5b27c1caed8342875ea31d8cb1ba6c9c07c39f0a
DeadLockMk1 <alexey.franzhev@gmail.com>
2015-07-23 18:58:06 +0300

    Added booster and bootstrap theme

--- yogurt/protected/config/main.php
+++ yogurt/protected/config/main.php
@@ -1,5 +1,4 @@
 <?php
-
 // uncomment the following to define a path alias
 // Yii::setPathOfAlias('local','path/to/local-folder');
 
@@ -7,31 +6,45 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'Yo!Gurt',
+    'theme'=>'bootstrap',
 
 	// preloading 'log' component
-	'preload'=>array('log'),
+	'preload'=>array(
+        'log',
+        'booster'
+    ),
+	'aliases' => array(
 
+    ),
 	// autoloading model and component classes
 	'import'=>array(
 		'application.models.*',
 		'application.components.*',
+        'ext.bootstrap-theme.widgets.*',
+        'ext.bootstrap-theme.helpers.*',
+        'ext.bootstrap-theme.behaviors.*',
 	),
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'2wbc31k9i87s84',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
+            'generatorPaths'=>array(
+                'ext.bootstrap-theme.gii',
+            ),
 		),
-		*/
 	),
 
 	// application components
 	'components'=>array(
+
+        'booster' => array(
+            'class' => 'application.extensions.booster.components.Booster',
+        ),
 
 		'user'=>array(
 			// enable cookie-based authentication
